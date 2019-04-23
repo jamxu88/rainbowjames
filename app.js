d7fd888f85247efb758a8429ff8c0c440adef382
James <43918220+jamxu88@users.noreply.github.com>
2019-04-23 18:02:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ client.on("ready", () => {
 client.on("message", (message) => {
   console.log(message.author.id)
   function change() {   
-    message.guild.members.get(289523788822085632).setNickname("James🌹");
-    message.guild.members.get(289523788822085632).setNickname("James");
-    message.guild.members.get(289523788822085632).setNickname("Jame");
-    message.guild.members.get(289523788822085632).setNickname("Jam");
-    message.guild.members.get(289523788822085632).setNickname("Ja");
-    message.guild.members.get(289523788822085632).setNickname("J");
-    message.guild.members.get(289523788822085632).setNickname("Ja");
-    message.guild.members.get(289523788822085632).setNickname("Jam");
-    message.guild.members.get(289523788822085632).setNickname("Jame");
-    message.guild.members.get(289523788822085632).setNickname("James");
+    message.guild.members.get(289523788822085632).setUsername("James🌹");
+    message.guild.members.get(289523788822085632).setUsername("James");
+    message.guild.members.get(289523788822085632).setUsername("Jame");
+    message.guild.members.get(289523788822085632).setUsername("Jam");
+    message.guild.members.get(289523788822085632).setUsername("Ja");
+    message.guild.members.get(289523788822085632).setUsername("J");
+    message.guild.members.get(289523788822085632).setUsername("Ja");
+    message.guild.members.get(289523788822085632).setUsername("Jam");
+    message.guild.members.get(289523788822085632).setUsername("Jame");
+    message.guild.members.get(289523788822085632).setUsername("James");
   }
   if (message.content.startsWith("<@530165753811304449>")) {
     setInterval(() => { change(); }, 1000);
