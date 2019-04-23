4d09890f7491f8301e1385b5d40dbc53c39326d8
James <43918220+jamxu88@users.noreply.github.com>
2019-04-23 18:59:50 -0400

    Update app.js

--- app.js
+++ app.js
@@ -32,48 +32,16 @@ client.on('message', async message => {
     let userid = message.guild.members.get("289523788822085632");
     userid.setNickname("J");
   }
-  function change6() {
-    let userid = message.guild.members.get("289523788822085632");
-    userid.setNickname("Ja");
-  }
-  function change7() {
-    let userid = message.guild.members.get("289523788822085632");
-    userid.setNickname("Jam");
-  }
-  function change8() {
-    let userid = message.guild.members.get("289523788822085632");
-    userid.setNickname("Jame");
-  }
-  function change9() {
-    let userid = message.guild.members.get("289523788822085632");
-    userid.setNickname("James");
-  }
   async function change10() {
     let userid = message.guild.members.get("289523788822085632");
     console.log("cycle");
     change0();
-    await sleep(2000);
+    await sleep(1500);
     change1();
-    await sleep(2000);
-    change2();
-    await sleep(2000);
-    change3();
-    await sleep(2000);
-    change4();
-    await sleep(2000);
-    change5();
-    await sleep(2000);
-    change4();
-    await sleep(2000);
-    change3();
-    await sleep(2000);
-    change2();
-    await sleep(2000);
-    change1();
-    await sleep(2000);
+    await sleep(1500);
   }
   if (message.content.startsWith("<@530165753811304449>")) {
-    setInterval(() => { change10(); }, 20000);
+    setInterval(() => { change10(); }, 3000);
   }else
   if (message.content.includes("rainbow")) {
     message.channel.send("no");
