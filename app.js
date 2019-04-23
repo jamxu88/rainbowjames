2bc519365e3a8b73330113d2996659b9238f8537
James <43918220+jamxu88@users.noreply.github.com>
2019-04-23 18:23:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,21 +5,63 @@ client.on("ready", () => {
 });
 client.on("message", (message) => {
   console.log(message.author.id)
-  function change() {
+  function change0() {
     let userid = message.guild.members.get("289523788822085632");
     userid.setNickname("James🌹");
+  }
+  function change1() {
+    let userid = message.guild.members.get("289523788822085632");
     userid.setNickname("James");
+  }
+  function change2() {
+    let userid = message.guild.members.get("289523788822085632");
     userid.setNickname("Jame");
+  }
+  function change3() {
+    let userid = message.guild.members.get("289523788822085632");
     userid.setNickname("Jam");
+  }
+  function change4() {
+    let userid = message.guild.members.get("289523788822085632");
     userid.setNickname("Ja");
+  }
+  function change5() {
+    let userid = message.guild.members.get("289523788822085632");
     userid.setNickname("J");
+  }
+  function change6() {
+    let userid = message.guild.members.get("289523788822085632");
     userid.setNickname("Ja");
+  }
+  function change7() {
+    let userid = message.guild.members.get("289523788822085632");
     userid.setNickname("Jam");
+  }
+  function change8() {
+    let userid = message.guild.members.get("289523788822085632");
     userid.setNickname("Jame");
+  }
+  function change9() {
+    let userid = message.guild.members.get("289523788822085632");
+    userid.setNickname("James");
+  }
+  function change10() {
+    let userid = message.guild.members.get("289523788822085632");
     userid.setNickname("James🌹");
   }
+  }
   if (message.content.startsWith("<@530165753811304449>")) {
-    setInterval(() => { change(); }, 2000);
+    setInterval(() => { change0(); }, 1000);
+    setInterval(() => { change1(); }, 2000);
+    setInterval(() => { change2(); }, 2000);
+    setInterval(() => { change3(); }, 2000);
+    setInterval(() => { change4(); }, 2000);
+    setInterval(() => { change5(); }, 2000);
+    setInterval(() => { change6(); }, 2000);
+    setInterval(() => { change7(); }, 2000);
+    setInterval(() => { change8(); }, 2000);
+    setInterval(() => { change9(); }, 2000);
+    setInterval(() => { change10(); }, 2000);
   }else
   if (message.content.includes("rainbow")) {
     message.channel.send("no");
